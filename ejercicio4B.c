72d5beb375140ac20033b805cde1b7e21f66b8a2
Fernando Sisul <fsisul@gmail.com>
2010-10-22 15:18:12 -0300

    Comentando ejercicios

--- ejercicio4B.c
+++ ejercicio4B.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-/* Imprime el numero que se le pasa por parametro */
+/* Imprime por pantalla el numero de hilo que esta ejecutando la 
+ * funcion.
+ * El numero es pasado por parametro */
 void *print(void *number) {
 
    int id = (int) number;
@@ -14,17 +16,19 @@ void *print(void *number) {
 int main() {
    
    pthread_t threads[2];
-
    int i;
-
+      
+   /* El proceso crea 2 hilos que ejecutan la funcion print */
    for(i=0; i<2; i++) {
       
       pthread_create(&threads[i], NULL, print, (void *) i);
 
    }
    
-   printf("Soy el proceso padre\n");
-
+   /* El proceso inicial muestra por pantalla quien es */
+   printf("Soy el proceso inicial\n");
+   
+   /* Espera que terminen los 2 hilos para finalizar la ejecucion */
    for(i=0; i<2; i++) {
       pthread_join(threads[i], NULL);
    }
