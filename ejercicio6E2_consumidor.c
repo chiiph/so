e869f878af4bab24a1a95456c4db9423e6cd2e3d
Fernando Sisul <fsisul@gmail.com>
2010-10-22 17:13:24 -0300

    Comentando ejercicios

--- ejercicio6E2_consumidor.c
+++ ejercicio6E2_consumidor.c
@@ -7,8 +7,10 @@
 #include <unistd.h>
 #include <semaphore.h>
 
+/* Defino el tamaño maximo del buffer */
 #define BUFFSIZE 10
 
+/* Defino la estructura del mensaje para la cola de mensajes */
 typedef struct messages{
    long mtype;
    char mtext[10];
@@ -17,33 +19,46 @@ typedef struct messages{
 int main(){
    
    msg_t msg;
-   int i,stat, msqid, shid;
    sem_t *full;
    sem_t *empty;
+   int i,stat, msqid, shid;
    
-   printf("(%d) Creating or accessing shared memory segment\n", getpid());
+   /* Se intenta crear el espacio para la cola de mensajes,
+    * si ya esta creada, se hace el attach de la misma */
+   printf("(%d) Creando o accediendo a la cola de mensajes\n", getpid());
 
-   if ((msqid = msgget(1234, (0666 | IPC_CREAT | IPC_EXCL)))  < 0){
-      if ((msqid = msgget(1234, 0666)) < 0){
-         printf("Error creating or accessing shared memory\n");
+   if ((msqid = msgget(1357, (IPC_CREAT | IPC_EXCL | 0666)))  < 0){
+      if ((msqid = msgget(1357, 0666)) < 0){
+         printf("Error al intentar crear o acceder a la cola de mensajes\n");
          exit(EXIT_FAILURE);
       }
    }
+   
+   printf("(%d) Cola de mensajes accedida\n", getpid());
+   sleep(1);
 
-   /* memoria compartida para los semaforos */
-   if ((shid = shmget(4567, sizeof(full)*2, (0666 | IPC_CREAT | IPC_EXCL))) < 0){
-      if ((shid = shmget(4567, sizeof(full)*2, 0666)) < 0){
+   /* Se intenta crear el espacio para la memoria compartida
+    * para los semaforos. Si ya esta creada, se hace el attach
+    * a la misma */
+   if ((shid = shmget(2468, sizeof(full)*2, (IPC_CREAT | IPC_EXCL | 0666))) < 0){
+      if ((shid = shmget(2468, sizeof(full)*2, 0666)) < 0){
          printf("Error al intentar crear o acceder a la memoria compartida\n");
          exit(EXIT_FAILURE);
       }
    }
 
+   printf("(%d) Memoria compartida accedida\n", getpid());
+   sleep(1);
+
+   /* Se asignan los espacios de memoria de los semaforos al segmento de 
+    * memoria compartida */
    full = (sem_t *)shmat(shid, 0, 0);
    empty = ((sem_t *)shmat(shid, 0, 0)) +1;
 
    
-   printf("(%d) Shared Memory accessed\n",getpid()); 
-   printf("(%d) Awaiting initialization message\n",getpid()); 
+   /* Se espera un mensaje que confirme la inicializacion de los semaforos */
+   printf("(%d) Esperando mensaje de inicializacion completada\n",getpid()); 
+
    stat = msgrcv(msqid, &msg, sizeof(msg) - sizeof(long), BUFFSIZE+1, 0); 
 
       if (stat < 0){
@@ -51,24 +66,37 @@ int main(){
          exit(EXIT_FAILURE);
       }
 
+   printf("(%d) Mensaje de inicializacion completada recibido\n", getpid());
+   printf("(%d) Comenzando el consumo de mensajes\n", getpid());
+
+   sleep(1);
+
    i=1;
    for (;;){      
-
+      
+      /* Espero el semaforo full. Si hay algun mensaje en la cola full
+       * tendra un valor entre 1 y BUFFSIZE y me dara el accesso */
       sem_wait(full);
 
+      /* Leo el mensaje que corresponda en la cola */
       stat = msgrcv(msqid, &msg, sizeof(msg) - sizeof(long), i, 0); 
 
       if (stat < 0){
-         printf("Error receiving message\n");
+         printf("Error recibiendo mensaje\n");
          exit(EXIT_FAILURE);
       }
 
-      printf("(%d) Message received: %s\n", getpid(), msg.mtext); 
+      printf("(%d) Mensaje recibido: %s\n", getpid(), msg.mtext); 
       
+      /* Aumento el valor del semaforo empty en 1, indicando que hay
+       * un mensaje menos en la cola */
       sem_post(empty);
 
+      /* espero 2 segundos */
       sleep(2);
-
+      
+      /* Aumento el indice que me indica de que lugar de la cola voy a leer
+       * el proximo mensaje */
       i=(i+1)%(BUFFSIZE+1);
       if (i == 0)
          i++;
--- ejercicio6E2_productor.c
+++ ejercicio6E2_productor.c
@@ -7,8 +7,10 @@
 #include <unistd.h>
 #include <semaphore.h>
 
+/* Defino el tamaño maximo del buffer */
 #define BUFFSIZE 10
 
+/* Defino la estructura del mensaje para la cola de mensajes */
 typedef struct messages{
    long mtype;
    char mtext[10];
@@ -19,45 +21,59 @@ int main(){
    msg_t msg;
    sem_t *full;
    sem_t *empty;
-
-   int i;
-
-   strcpy(msg.mtext,"PING");
-
-   int stat, msqid, shid;
+   int i, stat, msqid, shid;
 
    /* Se intenta crear el espacio para la cola de mensajes, 
-    * si ya esta creada, se hace el attach a la misma 
-    * */
-   printf("(%d) Creating or accessing shared memory segment\n", getpid());
+    * si ya esta creada, se hace el attach a la misma */
+   printf("(%d) Creando o accediendo a la cola de mensajes\n", getpid());
 
-   if ((msqid = msgget(1234, (0666 | IPC_CREAT | IPC_EXCL))) < 0){
-      if ((msqid = msgget(1234, 0666)) < 0){
+   if ((msqid = msgget(1357, (IPC_CREAT | IPC_EXCL | 0666))) < 0){
+      if ((msqid = msgget(1357, 0666)) < 0){
+         printf("Error al intentar crear o acceder a la cola de mensajes\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+
+   printf("(%d) Cola de mensajes accedida\n", getpid());
+   sleep(1);
+
+   /* Se intenta crear el espacio para la memoria compartida
+    * para los semaforos. Si ya esta creada, se hace el attach
+    * a la misma */
+   printf("(%d) Creando o accediendo a la memoria compartida\n", getpid());
+
+   if ((shid = shmget(2468, sizeof(full)*2, (0666 | IPC_CREAT | IPC_EXCL))) < 0){
+      if ((shid = shmget(2468, sizeof(full)*2, 0666)) < 0){
          printf("Error al intentar crear o acceder a la memoria compartida\n");
          exit(EXIT_FAILURE);
       }
    }
 
-   /* memoria compartida para el semaforo */
-   if ((shid = shmget(4567, sizeof(full)*2, (0666 | IPC_CREAT | IPC_EXCL))) < 0){
-      if ((shid = shmget(4567, sizeof(full)*2, 0666)) < 0){
-         printf("Error al intentar crear o acceder a la memoria compartida\n");
-         exit(EXIT_FAILURE);
-      }
-   }
+   printf("(%d) Memoria compartida  accedida\n", getpid());
+   sleep(1);
 
+   /* Se asignan los espacios de memoria de los semaforos al segmento de
+    * memoria compartida */
    full = (sem_t *)shmat(shid, 0, 0);
    empty = ((sem_t *)shmat(shid, 0, 0)) + 1;
 
+   /* Se inicializa los semaforos.
+    * empty se inicializa con el tamaño del buffer.
+    * full se inicializa con 0 */
+   printf("(%d) Inicializando los semaforos\n", getpid());
+   
    if ((sem_init(empty, 1, BUFFSIZE) == -1) || (sem_init(full, 1, 0) == -1)){
       printf("Error al inicializar los semaforos\n");
       exit(EXIT_FAILURE);
    }
 
 
-   printf("(%d) Shared memory accessed\n", getpid());
 
-   printf("(%d) pings\n", getpid());
+   /* Se envia un mensaje a la cola indicando que la inicializacion de
+    * los semaforos se completo */
+   printf("(%d) Enviando mensaje de inicializacion completada\n", getpid());
+   sleep(1);
+
    i=1;
    msg.mtype = BUFFSIZE+1;
    sprintf(msg.mtext, "%d", msg.mtype);
@@ -67,34 +83,44 @@ int main(){
          printf("Error al enviar mensaje\n");
          exit(EXIT_FAILURE);
       }
+    
+   printf("(%d) Mensaje de inicializacion completada enviado\n", getpid());
+   printf("(%d) Comenzando la produccion de mensajes\n", getpid());
    
-   printf("(%d) Message %d sent: %s\n", getpid(), i, msg.mtext);
-   sleep(3); 
+   sleep(1); 
+
    for(;;){
       
+      /* Espero el semaforo empty. Si hay lugar en la cola empty tendra un
+       * valor entre 1 y BUFFSIZE y me dara el acceso */
       sem_wait(empty);
 
+      /* Defino el mensaje que voy a enviar a la cola */
       msg.mtype = i;
       sprintf(msg.mtext, "%d", i);
 
+      /* Envio el mensaje a la cola */
       stat = msgsnd(msqid, &msg, sizeof(msg) - sizeof(long), 0);
       if (stat < 0){
          printf("Error al enviar mensaje\n");
          exit(EXIT_FAILURE);
       }
    
-      printf("(%d) Message %d sent: %s\n", getpid(), i, msg.mtext);
+      printf("(%d) Mensaje enviado: %s\n", getpid(), msg.mtext);
       
+      /* Aumento el valor del semaforo full en 1, indicando que hay un
+       * mensaje nuevo para leer en la cola */
       sem_post(full);
 
+      /* espero 1 segundo */
       sleep(1);
 
+      /* Aumento el indice que me indica en que lugar de la cola se pondra
+       * el proximo mensaje */
       i=(i + 1)%(BUFFSIZE+1);
       if (i == 0)
          i++;
-
    }
   
    exit(EXIT_SUCCESS);
-   
 }
